f3306dd73ea3afeb98c026d4ea33c2fb8fdb4c40
Naldo Colella <naldo.colella@gmail.com>
2018-11-08 00:42:00 -0200

    just to allow access-control local

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', function (req, res) {
   res.redirect('/produto/1321/checkout/6544')
 })
